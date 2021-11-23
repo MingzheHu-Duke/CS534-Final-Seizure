cdc86298aecfb389557710a8749a9aab052c2faf
warming151 <64031030+warming151@users.noreply.github.com>
2021-11-23 15:19:54 -0500

    Create CorrelationTemp_name.m

---
+++ preprocess/CorrelationTemp_name.m
@@ -0,0 +1,49 @@
+function mCorrs_T_name = CorrelationTemp_name()
+
+nChans = 16;
+mCorrs_T_name = []
+
+
+for i in 1:nChans    
+    mCorrs_T_name=append(mCorrs_T_name,"T_corrs_mean"+i);
+end
+
+for i in 1:nChans    
+    mCorrs_T_name=append(mCorrs_T_name,"T_corrs_std"+i);
+end
+
+for i in 1:nChans    
+    mCorrs_T_name=append(mCorrs_T_name,"T_corrs_sum"+i);
+end
+
+for i in 1:nChans    
+    mCorrs_T_name=append(mCorrs_T_name,"T_corrs_abs_sum"+i);
+end
+
+for i in 1:nChans    
+    mCorrs_T_name=append(mCorrs_T_name,"T_mean"+i);
+end
+
+for i in 1:nChans    
+    mCorrs_T_name=append(mCorrs_T_name,"T_abs_mean"+i);
+end
+
+for i in 1:nChans    
+    mCorrs_T_name=append(mCorrs_T_name,"T_std"+i);
+end
+
+for i in 1:nChans    
+    mCorrs_T_name=append(mCorrs_T_name,"T_sum"+i);
+end
+
+for i in 1:nChans    
+    mCorrs_T_name=append(mCorrs_T_name,"T_rms"+i);
+end
+
+for i in 1:nChans    
+    mCorrs_T_name=append(mCorrs_T_name,"T_kurtosis"+i);
+end
+
+for i in 1:nChans    
+    mCorrs_T_name=append(mCorrs_T_name,"T_skewness"+i);
+end
