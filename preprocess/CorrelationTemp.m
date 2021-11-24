481e132603d1295d9654e8f606c1992c24913374
warming151 <64031030+warming151@users.noreply.github.com>
2021-11-23 23:29:43 -0500

    1

--- preprocess/CorrelationTemp.m
+++ preprocess/CorrelationTemp.m
@@ -14,13 +14,13 @@ mCorrs_T(5,:) = corr_eig;
 
 
 %calculate summary feature
-mCorrs_T(6,:) = mean(data);
-mCorrs_T(7,:) = mean(abs(data));
-mCorrs_T(8,:) = std(data);
-mCorrs_T(9,:) = sum(data);
-mCorrs_T(10,:) = rms(data);
-mCorrs_T(11,:) = kurtosis(data);
-mCorrs_T(12,:) = skewness(data);
+mCorrs_T(6,:) = mean(data');
+mCorrs_T(7,:) = mean(abs(data'));
+mCorrs_T(8,:) = std(data');
+mCorrs_T(9,:) = sum(data');
+mCorrs_T(10,:) = rms(data');
+mCorrs_T(11,:) = kurtosis(data');
+mCorrs_T(12,:) = skewness(data');
 
 
 mCorrs_T = reshape(mCorrs_T,[1,192])
